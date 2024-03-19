0519fec87ec740ee6902bc82ab47a43e7f2121fc
Tharaka Rajasekara <124290593+TharakaRajasekara@users.noreply.github.com>
2024-03-19 07:42:28 +0530

    Add files via upload

---
+++ landing_page.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./landing_page.css";
+
+
+
+
+const LandingPage = ()=> {
+
+    return (
+     <>
+     <div className="main">
+     <div className="logo">
+        
+        </div>
+        <div className="logo_name">YOUR FITNES</div>
+
+        <div className ="menu">
+            <button className="menu_buttons">HOME</button>
+        </div>
+        <div className ="menu">
+            <button className="menu_buttons">SHOP CLOTHINGS</button>
+        </div>
+        <div className ="menu">
+            <button className="menu_buttons">LOCAL CLASSES</button>
+        </div>
+        <div className ="menu">
+            <button className="menu_buttons">ZUMBA APP</button>
+        </div>
+        <div className ="menu">
+            <button className="menu_buttons">INSTRUCTOR TRAININGS</button>
+        </div>
+        <div className ="menu">
+            <button className="menu_buttons">GALLERY</button>
+        </div>
+             
+        
+     </div>
+     <div className="body">body</div>
+     
+
+     
+     </>
+
+    )
+
+}
+
+export default LandingPage;
+
